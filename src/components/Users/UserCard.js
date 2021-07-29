45c4d9fc77833634620b5b3120794180e582303a
AlexanderPoskin <sumstor11@gmail.com>
2021-07-29 13:23:37 +0300

    adds scss files

--- src/components/Users/UserCard.js
+++ src/components/Users/UserCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import faker from 'faker';
+import './UserCard.scss';
 
 const UserCard = ({ name, username, website }) => {
   return (
-    <div className="user_card">
-      <div className="user_card_name">{name}</div>
-      <div className="user_card_username">{username}</div>
-      <div className="user_card_website">
+    <div className="user-card">
+      <div className="user-card__name">{name}</div>
+      <div className="user-card__username">{username}</div>
+      <div className="user-card__website">
         <a href={`https://${website}`} target="_blank" rel="noreferrer">
           {website}
         </a>
       </div>
       <div
-        className="user_avatar"
+        className="user-card__avatar"
         style={{ backgroundImage: `url(${faker.image.avatar()})` }}
       ></div>
     </div>
