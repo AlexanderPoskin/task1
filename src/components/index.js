f06d2289e4624c8411910505be70a0a1f7b60090
AlexanderPoskin <sumstor11@gmail.com>
2021-07-30 13:02:16 +0300

    small changes

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export { default as Quotes } from './Quotes/Quotes';
 export { default as Navigation } from '../components/Navigation';
 export { default as Users } from '../components/Users/Users';
 export { default as UsersFavorite } from '../components/Users/UsersFavorite';
-export {default as UserCard} from '../components/Users/UserCard';
+export { default as UserCard } from '../components/Users/UserCard';
+export { default as UserDetails } from '../components/Users/UserDetails';
--- src/reducers/indexReducers.js
+++ src/reducers/indexReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import users from './users';
 import favoriteUsers from './favoriteUsers';
+import detailsUsers from './detailsUsers';
 
 export default combineReducers({
   users,
   favoriteUsers,
+  detailsUsers,
 });
