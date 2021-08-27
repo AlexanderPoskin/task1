65cba780dd7090031c0b942b024474f7fb4fb6dc
AlexanderPoskin <sumstor11@gmail.com>
2021-08-27 22:31:25 +0300

    small updates

--- src/components/Users/UserSearch.js
+++ src/components/Users/UserSearch.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './UserSearch.scss';
 import { useDispatch } from 'react-redux';
 import { setSearch } from '../../reducers/users';
+import { debounce } from 'lodash';
 
 const UserSearch = () => {
   const dispatch = useDispatch();
-  const inputHandler = (e) => {
+
+  const inputHandler = debounce((e) => {
     dispatch(setSearch({ searched: e.target.value }));
+  }, 1000);
+
+  const clearSearch = () => {
+    dispatch(setSearch({ searched: '' }));
   };
 
   return (
@@ -14,8 +20,12 @@ const UserSearch = () => {
       <input
         className="user-search__input"
         type="text"
+        placeholder="Enter name or username"
         onChange={inputHandler}
       />
+      <button type="reset" onClick={clearSearch}>
+        X
+      </button>
     </form>
   );
 };
