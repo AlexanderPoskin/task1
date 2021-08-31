9e795fbc566108ce6fac7e85cb19c509c1676097
AlexanderPoskin <sumstor11@gmail.com>
2021-08-31 15:54:30 +0300

    last updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Header } from './components';
-
+import Routes from './routes/Routes';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes />
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
-import Routes from '../routes/Routes';
 
 class Menu extends Component {
   render() {
     return (
       <div className="transition">
         <Navigation />
-        <Routes />
       </div>
     );
   }
